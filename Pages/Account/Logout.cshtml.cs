eba6c23a343afa4f58104d0aef799b3bfc42debb
Wraami <chrisgower19@gmail.com>
2023-07-07 15:37:58 +0100

    feat: Implemented logout functionality and partial authorization for navbar pages
    Added logout functionality to the application to allow users to sign out, this works with user's remember settings.
    
    Implemented appropriate starting pages for users if they're signed in or not.
    
    Implemented partial authorization to restrict access to navbar pages for users who aren't signed in.

--- Pages/Account/Logout.cshtml.cs
+++ Pages/Account/Logout.cshtml.cs
@@ -1,3 +1,5 @@
+using DocDocGo.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,30 @@ namespace DocDocGo.Pages.Account
 {
     public class LogoutModel : PageModel
     {
-        public void OnGet()
+
+        private readonly SignInManager<UserModel> _signInManager;
+        private readonly ILogger<LogoutModel> _logger;
+
+        public LogoutModel(SignInManager<UserModel> signInManager, ILogger<LogoutModel> logger)
         {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> OnPost(string returnUrl = null)
+        {
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation("User logged out.");
+            if (returnUrl != null)
+            {
+                return LocalRedirect(returnUrl);
+            }
+            else
+            {
+                // This needs to be a redirect so that the browser performs a new
+                // request and the identity for the user gets updated.
+                return RedirectToPage();
+            }
         }
     }
 }
---
+++ Pages/Administrator/Settings.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DocDocGo.Pages.Administrator
+{
+    public class SettingsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
